e4f5334fd7b2c28a8ca9cd59ba506c124cac9150
Kuntuar <kuntuar92@mail.ru>
2022-11-23 23:00:27 +0600

    lesson-6 homework

--- src/test/java/tests/RegistrationPageObjectsTests.java
+++ src/test/java/tests/RegistrationPageObjectsTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
@@ -8,19 +9,22 @@ public class RegistrationPageObjectsTests extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
+
     void fillFormTest() {
-        String userName = "Кайрат",
-                lastName = " Кайратов",
-                 email = "kairat_test@testtt.comm",
+        Faker faker = new Faker();
+
+        String userName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                 email = faker.internet().emailAddress(),
                  gender = "Male",
-                 phone = "8777111111",
+                 phone = faker.phoneNumber().subscriberNumber(10),
                  birthDay = "30",
                  birthMonth = "July",
                  birthYear = "1900",
-                 subject = "qwerty",
+                 subject = faker.chuckNorris().fact(),
                  hobbies = "Sports",
                  jpfile = "img/homework.jpg",
-                 currentAddress = "Some address 1",
+                 currentAddress = faker.address().fullAddress(),
                  state = "Uttar Pradesh",
                  city = "Agra";
 
@@ -42,7 +46,7 @@ public class RegistrationPageObjectsTests extends TestBase {
 
 
         registrationPage.verifyResultModalAppears()
-                .verifyResult("Student Name" , userName + lastName)
+                .verifyResult("Student Name" , userName + " " + lastName)
                 .verifyResult("Student Email" , email)
                 .verifyResult("Gender" , gender)
                 .verifyResult("Mobile" , phone)
